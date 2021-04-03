3052f5e6b47aac088a254b904fc318df315052b5
fcaquard <fcaquard@student.s19.be>
2021-04-03 14:25:26 +0200

    atoi long number support

--- atoi.c
+++ atoi.c
@@ -6,13 +6,15 @@
 /*   By: fcaquard <fcaquard@student.s19.be>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/04/02 17:33:42 by fcaquard          #+#    #+#             */
-/*   Updated: 2021/04/02 19:58:23 by fcaquard         ###   ########.fr       */
+/*   Updated: 2021/04/03 14:21:46 by fcaquard         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include <stdio.h>
+
 int	ft_atoi(char *str);
 
-static int	loop_numbers(char *str, int dash, int nbr)
+static long long	loop_numbers(char *str, long long nbr)
 {
 	while (*str >= 48 && *str <= 57)
 	{
@@ -20,39 +22,41 @@ static int	loop_numbers(char *str, int dash, int nbr)
 		nbr += *str - '0';
 		str++;
 	}
-	if (dash == 1)
-		nbr *= -1;
-	return (nbr);
+	return nbr;
 }
 
-static int	loop_math_symbols(char *str, int negative, int nbr)
-{
-	if (*str == 45)
-	{
-		negative = 1;
-		nbr = loop_numbers(++str, negative, nbr);
-	}
-	else if (*str == 43)
-	{
-		nbr = loop_numbers(++str, negative, nbr);
-	}
-	else
-	{
-		nbr = loop_numbers(str, negative, nbr);
-	}
-	return (nbr);
-}
-
-static int	ft_isspace(char *str, int nbr)
+static char	*ft_isspace(char *str)
 {
 	while (*str == '\t' || *str == '\n' || *str == '\v'
 		|| *str == '\f' || *str == '\r' || *str == ' ')
 		str++;
-	nbr = (loop_math_symbols(str, 0, nbr));
-	return (nbr);
+	return (str);
 }
 
 int	ft_atoi(char *str)
 {
-	return (ft_isspace(str, 0));
+	long long poslimit = 999999999999999999;	
+	long long neglimit = -999999999999999999;
+	long long nbr;
+	int negative;
+
+	negative = 0;
+	str = ft_isspace(str);
+	if (*str == 45)
+		negative = 1;
+	
+	if (*str == 43 || *str == 45)
+		nbr = loop_numbers(++str, 0);
+	else
+		nbr = loop_numbers(str, 0);
+
+	if(negative == 1)
+		nbr = nbr * (-1);
+	
+	if(nbr > poslimit)
+		nbr = 0;
+	if(nbr < neglimit)
+		nbr = -1;
+		
+	return (nbr);
 }
