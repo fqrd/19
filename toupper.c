421a0a55ae6e8040fd8549b4560ab5f7be02ab54
fcaquard <fcaquard@student.s19.be>
2021-04-03 14:41:34 +0200

    special chars support

--- toupper.c
+++ toupper.c
@@ -6,20 +6,19 @@
 /*   By: fcaquard <fcaquard@student.s19.be>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/04/02 17:37:45 by fcaquard          #+#    #+#             */
-/*   Updated: 2021/04/02 17:48:08 by fcaquard         ###   ########.fr       */
+/*   Updated: 2021/04/03 14:40:35 by fcaquard         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-char	*toupper(char *str)
-{
-	int index;
 
-	index = 0;
-	while (str[index])
-	{
-		if (str[index] >= 'a' && str[index] <= 'z')
-			str[index] = str[index] - 32;
-		index++;
-	}
-	return (str);
+#include <stdio.h>
+int	ft_toupper(int c);
+
+int ft_toupper(int c)
+{
+	unsigned char ch;
+	ch = (unsigned char) c;
+	if (ch >= 'a' && ch <= 'z')
+		ch = ch - 32;
+	return (ch);
 }
