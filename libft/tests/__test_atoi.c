1711770c047571dc7f9afa6f8313c192b75b80c1
fcaquard <fcaquard@student.s19.be>
2021-04-04 21:45:11 +0200

    strchr && strrchr

--- libft/tests/__test_atoi.c
+++ libft/tests/__test_atoi.c
@@ -6,7 +6,7 @@
 /*   By: fcaquard <fcaquard@student.s19.be>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/04/02 19:06:30 by fcaquard          #+#    #+#             */
-/*   Updated: 2021/04/03 14:24:39 by fcaquard         ###   ########.fr       */
+/*   Updated: 2021/04/04 19:14:48 by fcaquard         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,13 +19,15 @@ int main()
 {
 	printf("------------\n----ATOI----\n------------\n");
 	int i = 0;
+	int lib;
+	int own;
 	char table[18][41] = {"0", "-10", "+30", "--150", "++190", "-2147483647", "+2147483647", "-21474836470", "21474836470", " 4", " 5e7", " 7 9", " - 5", " + 6", "-99999999999999999999999999", "99999999999999999999999999", "999999999999999999", "-999999999999999999"};
 
 	while (table[i] && i < 18)
 	{
-		int n_atoi = atoi(table[i]);
-		int n_ftatoi = ft_atoi(table[i]);
-		(n_atoi == n_ftatoi) ? printf("OK (%s) -> %d \n", table[i], n_ftatoi): printf("[ERROR] (%s) >> atoi(%d) != ft_atoi(%d)\n", table[i], n_atoi, n_ftatoi);
+		lib = atoi(table[i]);
+		own = ft_atoi(table[i]);
+		(lib == own) ? printf("OK (%s) -> %d \n", table[i], own): printf("[ERROR] (%s) >> atoi(%d) != ft_atoi(%d)\n", table[i], lib, own);
 		i++;
 	}
 	return (0);
--- libft/tests/__test_isalnum.c
+++ libft/tests/__test_isalnum.c
@@ -7,16 +7,18 @@ int main()
 {
 	printf("---------------\n----ISALNUM----\n---------------\n");
 	int i = 0;
+	int lib;
+	int own;
 	char string[57] = {"§W[eST7´[EdCF0d:hc§ir} fàWLDxié;kzL)yO'L0!:ùSm[Qe-"};
 
 	while (string[i] && i < 57)
 	{
-		int lib_isalnum = isalnum(string[i]);
-		int test_isalnum = ft_alnum(string[i]);
-		if(lib_isalnum == test_isalnum)
-			printf("OK (%c) -> %d \n", string[i], test_isalnum);
+		lib = isalnum(string[i]);
+		own = ft_alnum(string[i]);
+		if(lib == own)
+			printf("OK (%c) -> %d \n", string[i], own);
 		else
-			printf("[ERROR] (%c) >> isalnum(%d) != test_isalnum(%d)\n", string[i], lib_isalnum, test_isalnum);
+			printf("[ERROR] (%c) >> isalnum(%d) != own(%d)\n", string[i], lib, own);
 		i++;
 	}
 	return (0);
