a7ae0652192a66946453d8b371497c3ea2c89333
Florent Caquard <fcaquard@student.s19.be>
2021-04-09 22:40:24 +0200

    update gitignore

---
+++ libft/libtest.h
@@ -0,0 +1,115 @@
+#ifndef LIBTEST_H
+
+# define LIBTEST_H
+
+# include <stddef.h>
+
+
+
+////////////////
+
+///////TESTS
+
+/////////////////
+
+
+
+/**
+
+ * 	Numbers
+
+ */
+
+int		test_ft_atoi();
+
+
+
+/**
+
+ * 	Memory
+
+ */
+
+void	test_ft_bzero();
+
+void	*test_ft_memccpy();
+
+void	*test_ft_memchr();
+
+int		test_ft_memcmp();
+
+void	*test_ft_memcpy();
+
+void	*test_ft_memmove();
+
+void	*test_ft_memset();
+
+
+
+/**
+
+ *	Check Strings 
+
+ */
+
+int	test_ft_strlen();
+
+int		test_ft_strncmp();
+
+int	*test_ft_strnstr();
+
+int	*test_ft_strchr();
+
+int	*test_ft_strrchr();
+
+
+
+/**
+
+ *	Modify Strings 
+
+ */
+
+int	test_ft_strlcpy();
+
+int	test_ft_strlcat();
+
+
+
+/**
+
+ * Check ints
+
+ */
+
+int		test_ft_isalnum();
+
+int		test_ft_isalpha();
+
+int		test_ft_isascii();
+
+int		test_ft_isdigit();
+
+int		test_ft_isprint();
+
+
+
+/**
+
+ *	Modify ints 
+
+ */
+
+int		test_ft_tolower();
+
+int		test_ft_toupper();
+
+
+
+
+
+
+
+#endif
+
+
