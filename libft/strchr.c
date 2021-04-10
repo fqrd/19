745cb36081d620faeaa14c77379cf0c092559a1d
Florent Caquard <fcaquard@shi-r1-p1.s19.be>
2021-04-10 21:58:27 +0200

    strchr passes libft-unit-test~ but might need some optimisations

--- libft/strchr.c
+++ libft/strchr.c
@@ -6,17 +6,24 @@
 /*   By: fcaquard <fcaquard@student.s19.be>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/04/04 17:27:32 by fcaquard          #+#    #+#             */
-/*   Updated: 2021/04/09 16:17:45 by fcaquard         ###   ########.fr       */
+/*   Updated: 2021/04/10 21:52:27 by fcaquard         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 char	*ft_strchr(const char *s, int c)
 {
-	while (*s)
+	int i;
+	char *p = (char *)s;
+
+	i = 0;
+	while (p[i] != '\0')
 	{
-		if (*s == c)
-			return ((char *) s);
+		if (p[i] == c)
+			return ((char *)s);
+		i++;
 		s++;
 	}
+	if (p[i] == c)
+		return ((char *)s);
 	return (0);
 }
