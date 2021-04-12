07e48c68fcccea60c69b3765c524b46b075f5d16
fcaquard <fcaquard@student.s19.be>
2021-04-12 10:58:01 +0200

    fixing strdup

--- libft/strdup.c
+++ libft/strdup.c
@@ -6,7 +6,7 @@
 /*   By: fcaquard <fcaquard@student.s19.be>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/04/11 14:55:29 by fcaquard          #+#    #+#             */
-/*   Updated: 2021/04/12 10:56:16 by fcaquard         ###   ########.fr       */
+/*   Updated: 2021/04/12 10:57:46 by fcaquard         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,15 +17,17 @@ char	*ft_strdup(const char *s)
 {
 	char	*p;
 	size_t	n;
+	size_t	i;
 
 	n = 0;
+	i = 0;
 	while (s[n])
 		n++;
 	p = malloc(sizeof(char) * (n + 1));
 	if (!p)
 		return (NULL);
+	while (i++ < n)
+		p[i] = s[i];
 	p[n] = '\0';
-	while (--n >= 0)
-		p[n] = s[n];
 	return (p);
 }
