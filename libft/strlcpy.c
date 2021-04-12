6df49c381596bf9a3fe8e4017f291de40d69b4a1
fcaquard <fcaquard@student.s19.be>
2021-04-12 16:06:57 +0200

    strlcpy.c protected

--- libft/strlcpy.c
+++ libft/strlcpy.c
@@ -6,7 +6,7 @@
 /*   By: fcaquard <fcaquard@student.s19.be>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/04/04 12:34:59 by fcaquard          #+#    #+#             */
-/*   Updated: 2021/04/09 16:22:58 by fcaquard         ###   ########.fr       */
+/*   Updated: 2021/04/12 16:03:58 by fcaquard         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,7 +17,9 @@ size_t	ft_strlcpy(char *dest, const char *src, size_t size)
 {
 	size_t	index;
 	size_t	size_src;
-
+	
+	if(!dest && !src) 
+		return (0); 
 	index = 0;
 	size_src = ft_strlen(src);
 	if (size_src < size)
