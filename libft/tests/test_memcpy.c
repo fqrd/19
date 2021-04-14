a0fb0ffbc486628ae42600fe2ac3205933c3a8b3
fcaquard <fcaquard@student.s19.be>
2021-04-14 17:55:01 +0200

    test memcpy

--- libft/tests/test_memcpy.c
+++ libft/tests/test_memcpy.c
@@ -6,15 +6,45 @@
 int test_ft_memcpy ()
 {
 	printf("------------\n----MEMCPY----\n------------\n");
-
-	char ldest[16];
-	char odest[16];
-	char src[16] = "abcdefghijklmnop";
+	int error = 0;
+	int i = 0;
+	int *size = { 10, 20, 50, 200, 2000, -1, 0};
+	char ldest[723];
+	char odest[723];
+	char src[10][723] = {
+		"d:hc§",
+		"5LDxié;1z",
+		";1zL)y8'L0ù!:Sm[Qe-",
+		"§1W[eST7´[EdCF",
+		"software",
+		"YzRvK3KBXwHRuCYVscSia3OLLBMsGqDEfftxK8RA7e2o0Gsb0mFGmw1FH0tRqxcY7kmRHFBZ61XrMLIHP6OPfug7UYCIrmuFVlmGV2OTZs33PKseFlouilqVX6M8uwWQKJ17YNDqpf4tgIK94Zwi2LQ9nrARrq57Vk7pPNLggL0Xt5JdfGxt9u4LZnRlgyQFWlQesHUoZdDDWACFhN6LRZT1Xx6E73etJMU0jrY1h425dYLPE0vLyf3frmHMMDgizf95o4FmVAbzZXNJ7en3XdrjCkXuDT2FXEZG3qBojaQSqJISDSAc5TpRNsu4glwC7FiPja5AX9h0ADcsZhhtAY0Q0A91ov0Majk8onNTPz69Z7UIrMc0UajVAEe2inrZTRp0PCYgpd",
+		""
+	};
 	
-	printf("---\nbefore:\nlib: %p / %s\nown: %p / %s\nsrc: %p / %s\n", ldest, ldest, odest, odest, src, src);
+	while (src[i])
+	{
+		memcpy(ldest, src[i], size[i]);
+		ft_memcpy(odest, src[i], size[i]);
+		if (strcmp(ldest, odest) != 0)
+		{
+			printf("ERROR: (lib): |%s|	!=	|%s| :(yours)	/ |%s| :(src)\n", ldest, odest, src);
+			error = 1;
+		}
+		i++;
+	}
+
 	memcpy(NULL, NULL, 10);
 	ft_memcpy(NULL, NULL, 10);
-	printf("---\nafter:\nlib: %p / %s\nown: %p / %s\nsrc: %p / %s\n----", ldest, ldest, odest, odest, src, src);
+	if (strcmp(ldest, odest) != 0)
+	{
+		printf("ERROR: (lib): |%s|	!=	|%s| :(yours)	/ |%s| :(src)\n", ldest, odest, NULL);
+		error = 1;
+	}
+
+	if (!error)
+	{
+		printf("OK\n");
+	}
 	
 	return (0);
 }
