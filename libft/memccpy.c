c10383b5297b7601fd5eab09a2b762603d494fe0
fcaquard <fcaquard@student.s19.be>
2021-04-15 17:38:52 +0200

    rf stdio header

--- libft/memccpy.c
+++ libft/memccpy.c
@@ -6,12 +6,11 @@
 /*   By: fcaquard <fcaquard@student.s19.be>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/04/04 17:27:16 by fcaquard          #+#    #+#             */
-/*   Updated: 2021/04/15 17:00:04 by fcaquard         ###   ########.fr       */
+/*   Updated: 2021/04/15 17:05:50 by fcaquard         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <stddef.h>
-#include <stdio.h>
 
 void	*ft_memccpy(void *dest, const void *src, int c, size_t n)
 {
@@ -24,12 +23,10 @@ void	*ft_memccpy(void *dest, const void *src, int c, size_t n)
 	pdest = (unsigned char *) dest;
 	while (n--)
 	{	
-		printf("c: %c\n", psrc[i]);
 		pdest[i] = psrc[i];
 		if (psrc[i] == (unsigned char) c)
 		{	
 			pdest[++i] = '\0';
-			printf("res: %s\n", pdest);
 			return ((void *)(dest + i));
 		}
 		i++;
