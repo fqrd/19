d8ca88ee3c596466e66bfd88aabaa6b61c06e8ad
fcaquard <fcaquard@student.s19.be>
2021-04-21 17:16:02 +0200

    can't fix testLibft substr memory on ex#2

--- libft/ft_substr.c
+++ libft/ft_substr.c
@@ -6,7 +6,7 @@
 /*   By: fcaquard <fcaquard@student.s19.be>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/04/11 18:30:37 by fcaquard          #+#    #+#             */
-/*   Updated: 2021/04/19 14:58:37 by fcaquard         ###   ########.fr       */
+/*   Updated: 2021/04/21 17:14:13 by fcaquard         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,23 +19,25 @@ char	*ft_substr(char const *s, unsigned int start, size_t len)
 {
 	char			*ps;
 	char			*substring;
-	unsigned int	i;
+	size_t			i;
+	size_t			ml;
 
+	ml = 0;
 	i = 0;
-	substring = malloc(sizeof(char *) * (len + 1));
+	while (start + ml < ft_strlen(s) && ml < len)
+		ml++;
+	substring = malloc(sizeof(char *) * (ml + 1));
 	if(!substring || s == NULL || len == 0)
 		return (NULL);
-	if(start >= ft_strlen(s))
+	substring[ml] = '\0';
+	if (start > ft_strlen(s))
 		return (substring);
 	while (i++ < start)
 		s++;
 	i = 0;
 	ps = (char *) s;
-	while (len-- && *ps)
-	{
+	while (ml-- && *ps)
 		substring[i++] = *ps++;
-	}
-	substring[i] = '\0';
 	return (substring);
 }
 
