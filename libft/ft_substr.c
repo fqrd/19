7ed37b82f8462615293b5cef705760dca851db4b
fcaquard <fcaquard@student.s19.be>
2021-04-22 13:27:12 +0200

    using ft_strlcpy

--- libft/ft_substr.c
+++ libft/ft_substr.c
@@ -6,7 +6,7 @@
 /*   By: fcaquard <fcaquard@student.s19.be>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/04/11 18:30:37 by fcaquard          #+#    #+#             */
-/*   Updated: 2021/04/22 12:58:43 by fcaquard         ###   ########.fr       */
+/*   Updated: 2021/04/22 13:26:52 by fcaquard         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -29,14 +29,25 @@ char	*ft_substr(char const *s, unsigned int start, size_t len)
 	substring = malloc(sizeof(unsigned char *) * (llock));
 	if (!substring || s == NULL || len == 0)
 		return (NULL);
-	substring[llock] = '\0';
+	// substring[llock] = '\0';
 	if (start > ft_strlen(s))
 		return (substring);
 	while (i++ < start)
 		s++;
 	i = 0;
 	ps = (char *) s;
+	/*
 	while (llock-- && *ps)
 		substring[i++] = *ps++;
+	*/
+	ft_strlcpy(substring, ps, llock);
 	return (substring);
 }
+/*
+int main (void)
+{
+	char *str = ft_substr("i just want this part #############", 5, 20);
+	printf("main: |%s|\n", str);
+	return (0);
+}
+*/
