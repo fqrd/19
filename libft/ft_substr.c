f99bb28f212bf9a886e3b7c5bec2204073a38094
fcaquard <fcaquard@student.s19.be>
2021-04-22 14:07:29 +0200

    ft_substr - malloc (char*) instead of malloc(char)... really?

--- libft/ft_substr.c
+++ libft/ft_substr.c
@@ -6,12 +6,11 @@
 /*   By: fcaquard <fcaquard@student.s19.be>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/04/11 18:30:37 by fcaquard          #+#    #+#             */
-/*   Updated: 2021/04/22 13:26:52 by fcaquard         ###   ########.fr       */
+/*   Updated: 2021/04/22 13:55:45 by fcaquard         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <stddef.h>
-#include <stdio.h>
 #include <stdlib.h>
 #include "libft.h"
 
@@ -19,35 +18,18 @@ char	*ft_substr(char const *s, unsigned int start, size_t len)
 {
 	char			*ps;
 	char			*substring;
-	size_t			i;
 	size_t			llock;
 
 	llock = 0;
-	i = 0;
+	ps = (char *) s;
 	while (start + llock < ft_strlen(s) && llock < len)
 		llock++;
-	substring = malloc(sizeof(unsigned char *) * (llock));
+	substring = malloc(sizeof(char *) * (llock));
 	if (!substring || s == NULL || len == 0)
 		return (NULL);
-	// substring[llock] = '\0';
 	if (start > ft_strlen(s))
-		return (substring);
-	while (i++ < start)
-		s++;
-	i = 0;
-	ps = (char *) s;
-	/*
-	while (llock-- && *ps)
-		substring[i++] = *ps++;
-	*/
-	ft_strlcpy(substring, ps, llock);
+		ft_strlcpy(substring, "\0", 1);
+	else
+		ft_strlcpy(substring, ps + start, llock + 1);
 	return (substring);
 }
-/*
-int main (void)
-{
-	char *str = ft_substr("i just want this part #############", 5, 20);
-	printf("main: |%s|\n", str);
-	return (0);
-}
-*/
