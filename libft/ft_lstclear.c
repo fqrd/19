fcd9dec83c7f24b56ad7779fd4c37bbb05827282
fcaquard <fcaquard@student.s19.be>
2021-05-05 13:54:24 +0200

    lstclear error

--- libft/ft_lstclear.c
+++ libft/ft_lstclear.c
@@ -6,7 +6,7 @@
 /*   By: fcaquard <fcaquard@student.s19.be>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/05/05 13:20:34 by fcaquard          #+#    #+#             */
-/*   Updated: 2021/05/05 13:46:03 by fcaquard         ###   ########.fr       */
+/*   Updated: 2021/05/05 13:53:45 by fcaquard         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,17 +15,15 @@
 void	ft_lstclear(t_list **lst, void (*del) (void *))
 {
 	t_list	*tmp;
-	t_list	*p;
 
 	if (lst != NULL && *del != NULL)
 	{
-		p = *lst;
-		while (p)
+		while (*lst)
 		{
-			tmp = p-> next;
-			del(p-> content);
-			free (p);
-			p = tmp;
+			tmp = (*lst)-> next;
+			del((*lst)-> content);
+			free (*lst);
+			*lst = tmp;
 		}
 	}
 	lst = NULL;
