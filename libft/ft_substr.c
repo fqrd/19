ba7b8c4fb78c1d532ce1ad4b1aa5d3cabf5993ab
fcaquard <fcaquard@student.s19.be>
2021-05-10 17:27:46 +0200

    substr returned (NULL)

--- libft/ft_substr.c
+++ libft/ft_substr.c
@@ -6,7 +6,7 @@
 /*   By: fcaquard <fcaquard@student.s19.be>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/04/11 18:30:37 by fcaquard          #+#    #+#             */
-/*   Updated: 2021/05/10 16:06:23 by fcaquard         ###   ########.fr       */
+/*   Updated: 2021/05/10 16:18:03 by fcaquard         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,17 +20,19 @@ char	*ft_substr(char const *s, unsigned int start, size_t len)
 
 	llock = 0;
 	ps = (char *) s;
-	if (s && len != 0)
+	if (!s || len == 0 || start > ft_strlen(s))
 	{
-		while (start + llock < ft_strlen(s) && llock < len)
-			llock++;
+		substring = malloc(sizeof(char) * 1);
+		if (!substring)
+			return (NULL);
+		ft_strlcpy(substring, "\0", 1);
+		return (substring);
 	}
+	while (start + llock < ft_strlen(s) && llock < len)
+		llock++;
 	substring = malloc(sizeof(char) * (llock + 1));
 	if (!substring)
 		return (NULL);
-	if (start > ft_strlen(s) || len == 0 || !s)
-		ft_strlcpy(substring, "\0", 1);
-	else
-		ft_strlcpy(substring, ps + start, llock + 1);
+	ft_strlcpy(substring, ps + start, llock + 1);
 	return (substring);
 }
