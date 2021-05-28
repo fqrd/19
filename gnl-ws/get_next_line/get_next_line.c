7430ba4dbf9c47afb1fa2384b6a653d8b3cd5240
fcaquard <fcaquard@student.s19.be>
2021-05-28 17:50:41 +0200

    norminette

--- gnl-ws/get_next_line/get_next_line.c
+++ gnl-ws/get_next_line/get_next_line.c
@@ -6,127 +6,126 @@
 /*   By: fcaquard <fcaquard@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/05/15 19:26:35 by fcaquard          #+#    #+#             */
-/*   Updated: 2021/05/28 16:56:23 by fcaquard         ###   ########.fr       */
+/*   Updated: 2021/05/28 17:50:27 by fcaquard         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
 
-/*
-gcc -Werror -Wextra -Wall -D BUFFER_SIZE=32 get_next_line_utils.c get_next_line.c && ./a.out
+/**
+* gcc -Werror -Wextra -Wall -D BUFFER_SIZE=32 get_next_line_utils.c get_next_line.c && ./a.out
 */
 
-
-static void      find_char(t_status *status, char c)
+static void	find_char(t_status *s, char c)
 {
-    while (status->buffer[status->end])
-    {
-        // printf("c: %c\n",status->buffer[status->end]);
-        if(status->buffer[status->end] == c)
-        {
-            return ;
-        }
-        status->end++;
-    }
-    status->eob = 1;
-    return ;
+	while (s->buffer[s->end])
+	{
+		if (s->buffer[s->end] == c)
+			return ;
+		s->end++;
+	}
+	s->eob = 1;
+	return ;
 }
 
-int get_next_line(int fd, char **line)
-{    
-    if (fd < 0 || !line || BUFFER_SIZE <= 0)
-        return (-1);
-
-    static t_status *status;
-
-    if(!status)
-    {
-        status = new_status();
-    }
-
-    while (1)
-    {
-        if (status->populated)
-        {
-            find_char(status, '\n');
-            if(!status->eob)
-            {
-                // finds a line break
-                // adds the rest in front of it if any
-                // clears the rest
-                status->cut = ft_substr(status->buffer, status->start, status->end - status->start);
-                if(!status->cut)
-                    return (-1);
-                *line = ft_strjoin_empty(status->rest, status->cut, 1);
-                free(status->cut);
-                if(!*line)
-                    return (-1);
-                status->cut = NULL;
-                status->rest = NULL;
-                status->start = ++status->end;
-                // printf("LINE: |%s|\n", *line);
-                return (1);
-            }
-            else
-            {
-                // reached the end of the buffer
-                // concats the rest to the previous one if any
-                // does not clear the rest
-                status->cut = ft_substr(status->buffer, status->start, BUFFER_SIZE - status->start);
-                if(!status->cut)
-                    return (-1);
-                status->rest = ft_strjoin_empty(status->rest, status->cut, 1);
-                free(status->cut);
-                if(!status->rest)
-                    return (-1);
-                status->cut = NULL;
-                status->populated = 0;
-            }
-        }
-        else
-        {
-            /* CALL READ */
-            ft_bzero(status->buffer, BUFFER_SIZE);
-            status->read = read(fd, status->buffer, BUFFER_SIZE);
-            if (status->read < 0)
-            {
-                return (-1);
-            }
-            if(status->read && status->read == 0)
-            {
-                if(!status->rest || ft_strlen(status->rest) == 0)
-                {
-                    *line = malloc(sizeof(char) * 1);
-                    if(!*line)
-                        return (-1);
-                    *line[0] = '\0';
-                    if(status->rest != NULL)
-                    {
-                        free(status->rest);
-                    }
-                }
-                else
-                {
-                    *line = ft_strjoin_empty(status->rest, NULL, 1);
-                    if(!line)
-                    {
-                        free(*line);
-                        return (-1);
-                    }
-                }
-                // printf("EOF: |%s|\n", *line);
-                free(status);
-                return (0);
-            }
-            status->start = 0;
-            status->end = 0;
-            status->eob = 0;
-            status->populated = 1;
-            status->read = -1;
-        }
-    }
-   return (-1);
+static	int end_of_line(t_status *s, char **line)
+{
+	s->cut = ft_substr(s->buffer, s->start, s->end - s->start);
+	if (!s->cut)
+		return (0);
+	*line = ft_strjoin_empty(s->rest, s->cut, 1);
+	free(s->cut);
+	if (!*line)
+		return (0);
+	s->cut = NULL;
+	s->rest = NULL;
+	s->start = ++s->end;
+	printf("LINE: |%s|\n", *line);
+	return (1);
 }
+
+static	int end_of_buffer(t_status *s)
+{
+	s->cut = ft_substr(s->buffer, s->start, BUFFER_SIZE - s->start);
+	if (!s->cut)
+		return (0);
+	s->rest = ft_strjoin_empty(s->rest, s->cut, 1);
+	free(s->cut);
+	if (!s->rest)
+		return (0);
+	s->cut = NULL;
+	s->populated = 0;
+	return (1);
+}
+
+static int	end_of_file(t_status *s, char **line)
+{
+	if (!s->rest || ft_strlen(s->rest) == 0)
+	{
+		*line = malloc(sizeof(char) * 1);
+		if (!*line)
+			return (-1);
+		*line[0] = '\0';
+		if (s->rest != NULL)
+		{
+			free(s->rest);
+		}
+	}
+	else
+	{
+		*line = ft_strjoin_empty(s->rest, NULL, 1);
+		if (!line)
+			return (-1);
+	}
+	free(s);
+	printf("LINE: |%s|\n", *line);
+	return (0);
+}
+
+int	get_next_line(int fd, char **line)
+{
+	static t_status	*s;
+
+	if (fd < 0 || !line || BUFFER_SIZE <= 0)
+		return (-1);
+	if (!s)
+		s = new_status();
+	while (1)
+	{
+		if (s->populated)
+		{
+			find_char(s, '\n');
+			if (!s->eob)
+			{
+				if(!end_of_line(s, &(*line)))
+					return (-1);
+			}
+			else
+			{
+				if(!end_of_buffer(s))
+					return (-1);
+			}
+		}
+		else
+		{
+			ft_bzero(s->buffer, BUFFER_SIZE);
+			s->read = read(fd, s->buffer, BUFFER_SIZE);
+			if (s->read < 0)
+				return (-1);
+			if (s->read == 0)
+			{
+				return (end_of_file(s, &(*line)));
+			}
+			s->start = 0;
+			s->end = 0;
+			s->eob = 0;
+			s->populated = 1;
+			s->read = -1;
+		}
+	}
+	return (-1);
+}
+
 /*
 int main(void)
 {
