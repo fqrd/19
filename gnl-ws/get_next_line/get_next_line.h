bcb4a830b8d8810f28ebb1462a321425199d734a
fcaquard <fcaquard@student.s19.be>
2021-05-29 21:34:39 +0200

    Revert "norminette"
    This reverts commit 4b7cc24b59a44a22e7603687ccea1057667699e9.

--- gnl-ws/get_next_line/get_next_line.h
+++ gnl-ws/get_next_line/get_next_line.h
@@ -6,23 +6,27 @@
 # include <stdlib.h>
 # include <unistd.h>
 # include <stdio.h>
+// # include "wraloc.h"
 
 typedef struct s_list
 {
-	char	*buffer;
-	char	*cut;
-	char	*rest;
-	int		populated;
-	int		read;
-	size_t	start;
-	size_t	end;
-	int		eob;
-}				t_list;
+    // char    buffer[BUFFER_SIZE];
+    char    *buffer;
+    char    *cut;
+    char    *rest;
+    int     populated;
+    int     read;
+    size_t  start;
+    size_t  end;
+    int     eob;
+}                   t_list;
 
-t_list	*new_status(t_list *s);
-int		get_next_line(int fd, char **line);
-char	*substrjoin(t_list *s, size_t start, size_t end);
-void	ft_bzero(void *s, size_t n);
-size_t	ft_strlen(const char *str);
-char	*ft_strjoin_empty(char *s1, char *s2, int do_free);
+    t_list	*new_status(t_list *s);
+    int     get_next_line(int fd, char **line);
+    char    *substrjoin(t_list *s, size_t start, size_t end);
+    void	ft_bzero(void *s, size_t n);
+    size_t	ft_strlen(const char *str);
+    // char	*ft_substr(char const *s, unsigned int start, size_t len);
+    char	*ft_strjoin_empty(char *s1, char *s2, int do_free);
+    // size_t	ft_strlcpy(char *dest, const char *src, size_t size);
 #endif
