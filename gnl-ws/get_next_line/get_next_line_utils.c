7989d32d49bd62c7a2ed5e842fc5b98f57bef087
fcaquard <fcaquard@student.s19.be>
2021-05-30 15:31:21 +0200

    refactor

--- gnl-ws/get_next_line/get_next_line_utils.c
+++ gnl-ws/get_next_line/get_next_line_utils.c
@@ -6,7 +6,7 @@
 /*   By: fcaquard <fcaquard@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/05/15 19:26:41 by fcaquard          #+#    #+#             */
-/*   Updated: 2021/05/30 15:28:35 by fcaquard         ###   ########.fr       */
+/*   Updated: 2021/05/30 15:31:02 by fcaquard         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -71,11 +71,8 @@ char	*substrjoin(t_list *s, size_t start, size_t end)
 	{
 		if (s->rest && s->rest[index] != '\0' && index < restlen)
 			output[index] = s->rest[index];
-		else
-		{
-			if (s->buffer && s->buffer[start] && start < end)
-				output[index] = s->buffer[start++];
-		}
+		if (s->buffer && s->buffer[start] && start < end && index >= restlen)
+			output[index] = s->buffer[start++];
 		index++;
 	}
 	output[index] = '\0';
