42f015a563229e9419ed2dde51b93f09060e8aa3
fcaquard <fcaquard@student.s19.be>
2021-07-22 18:18:30 +0200

    leak problem solved

--- get_next_line/get_next_line.c
+++ get_next_line/get_next_line.c
@@ -6,7 +6,7 @@
 /*   By: fcaquard <fcaquard@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/06/01 20:00:17 by fcaquard          #+#    #+#             */
-/*   Updated: 2021/07/14 15:23:19 by fcaquard         ###   ########.fr       */
+/*   Updated: 2021/07/22 18:16:35 by fcaquard         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -33,7 +33,7 @@ static char	*action_on_buffer(t_list **s)
 {
 	if (find_char(&*s, '\n'))
 	{
-		(*s)->line = substrjoin(&*s, (*s)->start, ++(*s)->end,
+		(*s)->line = substrjoin(&*s, (*s)->start, (++(*s)->end - (*s)->start),
 				ft_strlen((*s)->rest));
 		if (!(*s)->line)
 			return (mfree(&*s, NULL));
@@ -45,7 +45,7 @@ static char	*action_on_buffer(t_list **s)
 		if ((*s)->start != (*s)->end)
 		{
 			(*s)->rest = substrjoin(&*s, (*s)->start,
-					(*s)->end, ft_strlen((*s)->rest));
+					((*s)->end - (*s)->start), ft_strlen((*s)->rest));
 			if (!(*s)->rest)
 				return (mfree(&*s, NULL));
 		}
@@ -58,10 +58,10 @@ static char	*load_buffer(int fd, t_list **s)
 {
 	int	res;
 
-	ft_bzero((*s)->buffer, BUFFER_SIZE);
 	res = read(fd, (*s)->buffer, BUFFER_SIZE);
 	if (res > 0)
 	{
+		(*s)->buffer[res] = '\0';
 		(*s)->start = 0;
 		(*s)->end = 0;
 		(*s)->populated = 1;
